0a88bb776d64a6dcd9c121b9b88f589ff9158e6c
vad <123727612+Fesh1fter@users.noreply.github.com>
2023-01-30 18:59:28 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/calc.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class calc {
+    @GetMapping("/sum/{a}/{b}")
+    public  Integer sumOfTwo(@PathVariable Integer a, @PathVariable Integer b)
+    {
+        return  a+b;
+    }
+    @GetMapping("/min/{a}/{b}")
+    public  Integer minOfTwo(@PathVariable Integer a, @PathVariable Integer b)
+    {
+        return  a-b;
+    }
+}
+    /*  ⣿⣿⣿⣿⣿⠿⠛⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠿⢿⣿⣿⣿
+        ⣿⣿⣿⣿⡇⠀⠈⣉⠛⢦⡈⠻⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠐⢉⡉⠀⠀⣿⣿⣿
+        ⣿⣿⣿⣿⣇⠀⠀⠈⠙⠒⠿⣆⠘⣿⣿⣿⣿⣿⠟⠀⣠⠶⠛⠋⠁⠉⢠⣿⣿⣿
+        ⣿⣿⣿⣿⣿⠀⢡⡀⠀⠀⠀⠘⣆⠘⣿⣿⣿⠏⠀⠔⠀⠀⠀⠀⡐⠀⢾⣿⣿⣿
+        ⣿⣿⣿⣿⡁⠀⠈⠻⢶⣤⣀⣀⣽⡆⠘⣿⠇⠀⢸⠀⢀⣠⡴⠞⠃⠀⠀⣿⣿⣿
+        ⣿⣿⣿⣿⣿⡦⣀⠀⠀⠉⠻⠟⢻⣿⠀⠀⠀⠃⠟⠛⠉⠁⠀⠀⢀⡄⣺⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣷⡘⡗⢦⣀⠀⠀⣸⣿⡇⠀⠀⠀⣀⡀⠀⢀⣤⢚⠏⣴⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣷⠰⠀⢻⠳⢾⣯⣸⣧⠀⠀⠀⠘⣡⡖⢻⠃⡌⣸⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⡇⢀⠀⢷⠀⡈⠻⣿⠀⠀⡠⠚⠀⢷⠃⢰⠁⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⡇⠀⢆⠘⡄⡁⢀⠉⠦⠊⣁⠴⢁⠄⠐⡟⠀⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⣧⠀⠘⣆⠈⢇⠨⠉⠰⠈⠁⢀⠏⠀⠰⠁⣰⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠹⡇⠈⢧⡀⢠⠀⣠⠋⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠙⠛⠋⠁⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠘⠿⠇⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⣀⣀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+     */
